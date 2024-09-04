ea4f9c689d3bdf95a753ecc06a0b6d1a5000741e
Luis Rios Coutino <lrioscoutino@gmail.com>
2024-09-03 20:59:19 -0600

    Tercer commit

--- proyecto2024B/urls.py
+++ proyecto2024B/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView
+)
 from users.views import (
     first_view,
     second_view,
@@ -26,5 +30,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', first_view, name="first-view"),
     path('users2/', second_view, name="second-view"),
-    path('', base_view, name="base-view"),
+    path('', LoginView.as_view(template_name="base.html"), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 ]
