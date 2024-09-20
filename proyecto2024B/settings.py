d0e371b7e97a984d00f1a2c79afcf51952305d9a
Luis Rios Coutino <lrioscoutino@gmail.com>
2024-09-19 19:57:32 -0600

    mysql-django-extensions-data

--- proyecto2024B/settings.py
+++ proyecto2024B/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "corsheaders",
     'rest_framework',
     'drf_yasg',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -78,12 +79,26 @@ WSGI_APPLICATION = 'proyecto2024B.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": "products_db",
+        "USER": "beta",
+        "PASSWORD": "12345678",
+        "HOST": "172.18.0.1",
+        "PORT": "3306",
     }
 }
+
+
+
 
 
 # Password validation
